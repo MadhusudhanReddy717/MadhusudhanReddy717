08b18694af64bc76c98d20049ea391d2719b9c9f
MadhusudhanReddy717 <113969054+MadhusudhanReddy717@users.noreply.github.com>
2025-08-12 20:08:11 +0530

    Add files via upload

---
+++ Q23Consumer/src/main/java/com/example/q23consumer/Q23ConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.q23consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q23ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Q23ConsumerApplication.class, args);
+    }
+}
+//http://localhost:8082/rides
